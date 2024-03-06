f69b33d38de187888293124ada2df8d442e219cd
GALLIC-A <axel.gallic@orange.fr>
2024-03-06 15:10:35 +0100

    Implémentation de la prise de photo incroyable, affichage et sauvegarde dans le stockage interne

--- app/src/main/java/com/example/mygesplus/FullDescriptionCourseActivity.kt
+++ app/src/main/java/com/example/mygesplus/FullDescriptionCourseActivity.kt
@@ -1,15 +1,34 @@
 package com.example.mygesplus
 
+import android.content.ActivityNotFoundException
+import android.content.ContentValues
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import com.example.mygesplus.model.Course
 import com.example.mygesplus.view.FullCourseDescriptionView
 import com.google.gson.Gson
+import java.io.IOException
 
 
 class FullDescriptionCourseActivity : ComponentActivity() {
+
+    // Gestion de la photo
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    var photoDuCours by mutableStateOf<Bitmap?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,8 +39,88 @@ class FullDescriptionCourseActivity : ComponentActivity() {
         Log.wtf("courseSelected class", course.description)
 
         setContent {
-            FullCourseDescriptionView(course)
+            FullCourseDescriptionView(
+                course,
+                this@FullDescriptionCourseActivity::dispatchTakePictureIntent,
+                photoDuCours
+            )
         }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // ERREUR
+            Toast.makeText(this, "Une erreur est survenue lors de l'ouverture de la caméra.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            photoDuCours = data?.extras?.get("data") as Bitmap
+
+            // Quand l'image apparaît, le bouton devient invisible.
+            // on pourrait le désactiver si on avait + de place.
+
+            // Sauvegarde de l'image dans le stockage interne de l'appareil
+            saveBitmapImage(photoDuCours!!)
+        }
+    }
+
+    private fun saveBitmapImage(bitmap: Bitmap) {
+        val timeStamp = System.currentTimeMillis()
+        val values = ContentValues().apply {
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.DATE_ADDED, timeStamp / 1000)
+            put(MediaStore.Images.Media.DATE_TAKEN, timeStamp)
+        }
+
+        var imageFilePath: String? = null
+
+        // Version supérieure ou égale à la version Q (Android 10)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES + "/" + getString(R.string.app_name))
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+
+            val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            uri?.let { imageUri ->
+                try {
+                    contentResolver.openOutputStream(imageUri)?.use{ outputStream ->
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                    }
+                    values.put(MediaStore.Images.Media.IS_PENDING, false)
+                    contentResolver.update(imageUri, values, null, null)
+
+                    imageFilePath = getImageFilePathFromUri(imageUri)
+
+                    persistPathInRoomDatabase(imageFilePath!!)
+
+                } catch (e: IOException){
+                    Toast.makeText(this, "Une erreur est survenue lors de la sauvegarde de l'image sur votre appareil.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Toast.makeText(this, "La version d'Android que votre appareil utilise ne permet pas l'enregistrement de l'image...", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun persistPathInRoomDatabase(imagePath: String) {
+        // Logique de sauvegarde du chemin de l'image dans la base de données ROOM.
+    }
+
+    private fun getImageFilePathFromUri(uri: Uri): String? {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        contentResolver.query(uri, projection, null, null, null)?.use { cursor ->
+            if(cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                return cursor.getString(columnIndex)
+            }
+        }
+        return null
     }
 }
 
--- app/src/main/java/com/example/mygesplus/view/FullCourseDescriptionView.kt
+++ app/src/main/java/com/example/mygesplus/view/FullCourseDescriptionView.kt
@@ -1,23 +1,36 @@
 package com.example.mygesplus.view
 
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import com.example.mygesplus.model.Course
 import com.example.mygesplus.ui.theme.CtxMain
 
 @Composable
 fun FullCourseDescriptionView(
-    course: Course
+    course: Course,
+    dispatchTakePictureIntent: () -> Unit,
+    photoDuCours: Bitmap?
 ) {
+
+    // Définir un état pour gérer la visibilité du bouton
+    val buttonVisible = remember { mutableStateOf(true) }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -31,4 +44,30 @@ fun FullCourseDescriptionView(
             Text(text = course.description + " " + course.date + " " + course.isPresentiel.toString())
         }
     }
+
+    // Bouton pour prendre une photo
+    if (buttonVisible.value) {
+        Button(
+            onClick = {
+                dispatchTakePictureIntent() // Appel de la fonction de prise de photo
+            }
+        ) {
+            Text("Prendre une photo")
+        }
+    }
+
+    // La photo du cours n'apparaît que si elle existe.
+    photoDuCours?.let {
+        Image(
+            bitmap = photoDuCours.asImageBitmap(),
+            contentDescription = null, // Ajoutez une description si nécessaire
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+
+        // Rendre le bouton invisible une fois que l'image est affichée
+        buttonVisible.value = false
+    }
+
 }

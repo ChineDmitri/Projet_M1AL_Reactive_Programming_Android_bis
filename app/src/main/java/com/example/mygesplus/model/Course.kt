89c8486f6a8806c1013c1e4bec6019819d94b60c
ChineDmitri <mr.dima11@gmail.com>
2024-03-06 15:06:42 +0100

    Add dependencies

--- app/src/main/java/com/example/mygesplus/model/Course.kt
+++ app/src/main/java/com/example/mygesplus/model/Course.kt
@@ -1,16 +1,15 @@
 package com.example.mygesplus.model
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.android.parcel.Parcelize
+import java.sql.Timestamp
 
 @Entity(tableName = "cours")
 data class Course(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "nom") val nom: String,
-    @ColumnInfo(name = "date") val date: String,
+    @ColumnInfo(name = "date") val date: Timestamp,
     @ColumnInfo(name = "heure_debut") val heureDebut: String,
     @ColumnInfo(name = "heure_fin") val heureFin: String,
     @ColumnInfo(name = "description") val description: String,

3676709827d98c627f66b00232b9c0e6b04abc84
ChineDmitri <mr.dima11@gmail.com>
2024-03-06 14:09:15 +0100

    ViewModel ok

--- app/src/main/java/com/example/mygesplus/MainActivity.kt
+++ app/src/main/java/com/example/mygesplus/MainActivity.kt
@@ -1,34 +1,35 @@
 package com.example.mygesplus
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mygesplus.model.Course
 import com.example.mygesplus.view.CourseItemView
 import com.example.mygesplus.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
-    /*private val viewModel: SharedViewModel by viewModels()*/
-    private val viewModel: MainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                itemsIndexed(viewModel.courses) { index, cours ->
-                    Log.wtf("IDX::CourseItemView", index.toString())
-                    CourseItemView(cours) /*{
-                        viewModel.onCourseSelected(cours)
-                    }*/
-                }
-            }
+            val viewModel: MainViewModel = viewModel()
+
+            MainScreen(viewModel.courses.value)
+        }
+    }
+}
+
+@Composable
+fun MainScreen(courses: List<Course>) {
+    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        itemsIndexed(courses) { index, course ->
+            CourseItemView(course)
         }
     }
 }
--- app/src/main/java/com/example/mygesplus/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mygesplus/viewmodel/MainViewModel.kt
@@ -1,27 +1,32 @@
 package com.example.mygesplus.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.mygesplus.model.Course
 
 class MainViewModel: ViewModel() {
-    public val courses: List<Course> = listOf(
-        Course(
-            1,
-            "Cours 1",
-            "01/01/2022",
-            "8h00",
-            "8h00",
-            "Super Android course",
-            true,
-        ),
-        Course(
-            2,
-            "Cours 2 : Rust", "02/01/2022",
-            "12h00",
-            "13h00",
-            "Super Rustaman course",
-            true,
-        ),
+    private val _courses: MutableState<List<Course>> = mutableStateOf(
+        listOf(
+            Course(
+                1,
+                "Cours 1",
+                "01/01/2022",
+                "8h00",
+                "8h00",
+                "Super Android course",
+                true,
+            ),
+            Course(
+                2,
+                "Cours 2 : Rust", "02/01/2022",
+                "12h00",
+                "13h00",
+                "Super Rustaman course",
+                true,
+            ),
+        )
     )
 
+    val courses: MutableState<List<Course>> get() = _courses
 }

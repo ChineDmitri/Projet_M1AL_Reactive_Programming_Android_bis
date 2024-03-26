f13dbff4f2e8819e5101839402aae4822b083ce1
ChineDmitri <mr.dima11@gmail.com>
2024-03-27 00:35:18 +0100

    Add operator ternary

--- app/src/main/java/com/example/mygesplus/view/CourseItemView.kt
+++ app/src/main/java/com/example/mygesplus/view/CourseItemView.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,10 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mygesplus.FullDescriptionCourseActivity
@@ -113,7 +110,7 @@ fun CourseItemView(
 
                 )
                 {
-                    Text(text = course.isPresentiel.toString(),
+                    Text(text = if (course.isPresentiel) "Presentiel" else "Distanciel",
                         style = TextStyle(color = Color.White, fontSize = 18.sp))
                 }
             }
